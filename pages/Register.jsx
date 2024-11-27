a1223f119120810281bf3bdd22b4c7abbb4748ae
imana173 <imana173@gmail.com>
2024-11-27 15:40:23 +0100

    Commit Styles

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
-import '../styles/Login.css';
+import '../src/style/login.css';
 
 function Register() {
   const navigate = useNavigate();
