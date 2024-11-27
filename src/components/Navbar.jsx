fdf743b2a431c9da3c34c9972d868ed0e39653fe
imana173 <imana173@gmail.com>
2024-11-27 15:20:15 +0100

    Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
-import '../style/Navbar.css';
-
+import "../style/Navbar.css"
 const Navbar = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
